7564dcc81285327e1119a1e81d24e95387b4c683
Mitesh Shah <mitesh@miteshshah.com>
2021-12-09 17:16:54 +0530

    strip newlines from xml

--- src/lambdaisland/reitit_jaatya/sitemap.clj
+++ src/lambdaisland/reitit_jaatya/sitemap.clj
@@ -9,18 +9,19 @@
 
 (defn generate [site-url paths & [{:keys [trailing-slash ignored-files sitemap-ignored-paths]
                                    :or {trailing-slash false}}]]
-  (with-out-str
-    (emit
-      {:tag :urlset
-       :attrs {:xmlns "http://www.sitemaps.org/schemas/sitemap/0.9"}
-       :content
-       (for [path paths]
-         {:tag :url
-          :content
-          [{:tag :loc
-            :content [(str site-url
-                           path
-                           (when (and trailing-slash (not (string/ends-with? path "/")))
-                             "/"))]}
-           #_{:tag :lastmod
-            :content [(-> f (.lastModified) (Date.) format-date)]}]})})))
+  (let [xml (with-out-str
+              (emit
+               {:tag :urlset
+                :attrs {:xmlns "http://www.sitemaps.org/schemas/sitemap/0.9"}
+                :content
+                (for [path paths]
+                  {:tag :url
+                   :content
+                   [{:tag :loc
+                     :content [(str site-url
+                                    path
+                                    (when (and trailing-slash (not (string/ends-with? path "/")))
+                                      "/"))]}
+                    #_{:tag :lastmod
+                       :content [(-> f (.lastModified) (Date.) format-date)]}]})}))]
+    (string/replace xml #"\n" "")))
